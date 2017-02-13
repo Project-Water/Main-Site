708cf7507c381826afadf19a1efbc559fa3785e3
Alex Taffe <alex.taffe@gmail.com>
2017-02-12 23:03:50 -0500

    Update text

--- dodgeball.php
+++ dodgeball.php
@@ -1,71 +1,42 @@
 <?php include 'template-upper.php'; ?>
-    <h1 class="subPageTitle">NA Dodgeball Tournaments</h1>
-    <hr>
-    <h2 class="subPageTitle">NASH</h2>
-    <div class="countDownContainer">
-        <h2 class="countDownTitle">GET READY TO PLAY IN</h2>
-        <div class="countDownNASH">
-        </div>
-        <div class='donateButton donateButtonInverted registerButtonNASH'>
-            <a href='http://goo.gl/forms/PvmL1o8hBB' target='_blank'>
-                <div class='donateButtonText donateButtonTextInverted'>REGISTER</div>
-            </a>
-        </div>
-		
+<h1 class="subPageTitle">NA Dodgeball Tournaments</h1>
+<hr>
+<h2 class="subPageTitle">2017</h2>
+<div class="countDownContainer">
+    <h2 class="countDownTitle">REGISTER IN</h2>
+    <div class="countDownNASH">
+    </div>
+    <div class='donateButton donateButtonInverted registerButtonNASH'>
+        <a href='http://goo.gl/forms/PvmL1o8hBB' target='_blank'>
+            <div class='donateButtonText donateButtonTextInverted'>REGISTER</div>
+        </a>
+    </div>
+
+</div>
+<br>
+<div class="embed-responsive embed-responsive-16by9 dodgeVideo">
+    <embed id="video" src="https://www.youtube-nocookie.com/embed/EYeKDpXzaCw?theme=light&rel=0&autoplay=0&showinfo=0&origin=https://naprojectwater.com" frameborder="0" allowfullscreen class="embed-responsive-item">
     </div>
     <br>
-    <div class="embed-responsive embed-responsive-16by9 dodgeVideo">
-        <embed id="video" src="https://www.youtube-nocookie.com/embed/p2X-_aMzXBU?theme=light&rel=0&autoplay=0&showinfo=0&origin=https://naprojectwater.com" frameborder="0" allowfullscreen class="embed-responsive-item">
-    </div>
-    <br>
-    <article class="dodgeBallArticle">
-        <p>Please have <strong>1 team member</strong> submit this form for your team. The cost is <strong>$30/player</strong>, and includes a t-shirt, pizza, and an all-night tournament for all players. All money and signed permission forms are due no later than <strong>Thursday, February 4th at 2:15 P.M.</strong> </p>
+    <article class="dodgeBallArtic>le">
+        <p>Check back here on <strong>Monday, February 20th at 8:10 AM</strong> to register your team for the 2017 NA Dodgeball Tournament.
+            Teams must have 6 players and can have one additional (optional) coach/sub.</p>
+
+            <p>The <strong>2017 NA Dodgeball Tournament</strong> is scheduled for all school day at NAI and NASH on <strong>April 13th, 2017,</strong> the day before spring break.
+                As part of this tournament, ALL NAI and NASH participants will have <strong>Evening Championships</strong> at NASH, from <strong>7-10 PM.</strong>
+                The cost is <strong>$30/player,</strong> and includes a t-shirt, pizza, all-day tournament admission, and evening championship for all players.
+                During registration, teams can select the <strong>Pizza Supreme</strong> package. For <strong>$5/player more</strong>, your entire team can have pop and double pizza on game day.</p>
 
         <!--<p>Download parent signature form <a href="pdf/ParentSignatureForm.pdf" target="_blank">here</a>.
     </p>-->
 
-        <p>The NASH Dodgeball Tournament is all night on starting on <strong>March 23rd, 2016 at 9:00 P.M.</strong> Please make sure all players can be present throughout the duration of the tournament, and have arranged for rides home at 7:00am on March 24th.</p>
-        <p>
-            <strong>
+    <!--<p>The NASH Dodgeball Tournament is all night on starting on <strong>March 23rd, 2016 at 9:00 P.M.</strong> Please make sure all players can be present throughout the duration of the tournament, and have arranged for rides home at 7:00am on March 24th.</p>-->
+        <!--<p>
+        <strong>
             If you are interested in FREE tickets, <a href="/freetickets">click here</a>.
         </strong>
-        </p>
-        <h3 class="text-center">Spectator Registration</h3>
-        <div class="donateButton"><a href="http://goo.gl/forms/Y8vuiyovqx" target="_blank"><div class="donateButtonText">REGISTER</div></a></div>
-    </article>
-    <hr>
-    <h2 class="subPageTitle">NAI</h2>
-    <div class="countDownContainer">
-        <h2 class="countDownTitle">GET READY TO PLAY IN</h2>
-        <div class="countDownNAI">
-
-        </div>
-        <div class='donateButton donateButtonInverted registerButtonNAI'>
-            <!--Dang fool, register early and you don't get in-->
-            <a href='http://goo.gl/forms/fymd88nkuf' target='_blank'>
-                <div class='donateButtonText donateButtonTextInverted'>REGISTER</div>
-            </a>
-        </div>
-    </div>
-    <br>
-    <div class="embed-responsive embed-responsive-16by9 dodgeVideo">
-        <embed id="video" src="https://www.youtube-nocookie.com/embed/jqdeunM31qs?theme=light&rel=0&autoplay=0&showinfo=0&origin=https://naprojectwater.com" frameborder="0" allowfullscreen class="embed-responsive-item">
-    </div>
-    <br>
-    <article class="dodgeBallArticle">
-        <p>Please have <strong>1 team member</strong> submit this form for your team. The cost is <strong>$30/player</strong>, and includes a t-shirt, pizza, and an all-day tournament for all players. All money and signed permission forms are due no later than <strong>Thursday, February 26th at 2:15 P.M.</strong> in the counseling office.</p>
-
-        <p>Download parent signature form <a href="pdf/NAIPermissionFormFINAL.pdf" target="_blank">here</a>.
-    </p>
-
-        <p>The NAI Dodgeball Tournament is starting on <strong>March 23rd, 2016</strong>. Please make sure all players can be present throughout the duration of the tournament</p>
-        <p>
-            The online registration form will appear here along with instructions for submitting money and permission forms in the upcoming weeks.
-        </p>
-        <p>
-            <strong>
-            If you are interested in FREE tickets, <a href="/freetickets">click here</a>.
-        </strong>
-        </p>
+    </p>-->
+        <!--<h3 class="text-center">Spectator Registration</h3>
+        <div class="donateButton"><a href="http://goo.gl/forms/Y8vuiyovqx" target="_blank"><div class="donateButtonText">REGISTER</div></a></div>-->
     </article>
     <?php include 'template-lower.php'; ?>
--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 function createCounter() {
 
     $('.countDownNASH').countdown({
-        date: "March 23, 2016 21:00:00",
+        date: "February 20, 2017 8:10:00",
         render: function (data) {
 
             var el = $(this.el);
@@ -14,23 +14,6 @@ function createCounter() {
         onEnd: function () {
             $(".countDownNASH").css("display", "none");
             $(".registerButtonNASH").css("display", "block");
-            $(".countDownTitle").css("display", "none");
-        }
-    });
-    $('.countDownNAI').countdown({
-        date: "March 23, 2016 08:10:00",
-        render: function (data) {
-
-            var el = $(this.el);
-            el.empty()
-                .append(this.leadingZeros(data.days, 1) + "&nbsp;days,&nbsp;")
-                .append(this.leadingZeros(data.hours, 1) + "&nbsp;hours, ")
-                .append(this.leadingZeros(data.min, 1) + "&nbsp;mins,&nbsp;")
-                .append(this.leadingZeros(data.sec, 1) + "&nbsp;secs");
-        },
-        onEnd: function () {
-            $(".countDownNAI").css("display", "none");
-            $(".registerButtonNAI").css("display", "block");
             $(".countDownTitle").css("display", "none");
         }
     });
