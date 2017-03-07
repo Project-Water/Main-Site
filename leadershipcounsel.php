1d4aadb172c43316df82b52f84c4769657b1471d
Alex Taffe <alex.taffe@gmail.com>
2017-03-06 21:59:49 -0500

    Create Josh's fake "Leadership counsel" page

---
+++ leadershipcounsel.php
@@ -0,0 +1,31 @@
+<?php include 'template-upper.php'; ?>
+    <h1 class="subPageTitle">2017 Global Leadership Counsel</h1>
+    <hr>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This year, a select group of student representatives have been chosen as part of the 2017 Project Water Global Leadership Counsel. Students selected are recognized their qualities of leadership, patriotism, scholarship, service, courage, and honor. These leaders were selected based on current service projects, along with an application essay and interview with the Project Water Global Leadership Committee. Every year, representatives meet for a special ceremony and attend Project Water's annual fundraiser sporting event. This year's event is scheduled for April 13, 2017 at North Allegheny High School.</p><br>
+    <div class="text-center">
+        <h4>Leadership Team Representatives:</h4>
+
+        <h5>Central Catholic</h5>
+        Elias Reay<br>
+        Ethan Reay
+        <br>
+        <h5>Fox Chapel</h5>
+        Michael Bliss
+        <br>
+        <h5>Shaler</h5>
+        Livya Robinson<br>
+        Ceari Robinson
+        <br>
+        <h5>North Hills</h5>
+        Caitlyn Gazica<br>
+        Jacob Traeger
+        <br>
+        <h5>Hampton</h5>
+        David Puccio
+        <br>
+        <h5>Allegheny County</h5>
+        Zachary Hanson<br>
+        Thomas Rice
+        <br>
+    </div>
+    <?php include 'template-lower.php'; ?>
